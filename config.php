91056256e321822b026f1d4d4a92cc37a4cbe3df
Rehanul Ahmed <79300086+reh1548@users.noreply.github.com>
2022-12-13 10:33:59 +0600

    Project

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 
+// you need to install Google Clinet API library in your project directory to get the vendor/autoload.php
 require_once 'vendor/autoload.php';
 
 session_start();
 
 // init configuration
-$clientID = '417806044441-mm1ti3dgu5c1vu20i86id7f0nugqn871.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://localhost/Vault-Tec-Hardware-Store(2)/better/controllers/loginGoogle.php';
+$clientID = 'your_clientID';
+$clientSecret = 'your_clientSecret';
+$redirectUri = 'your_redirectUrl';
 
 // create Client Request to access Google API
 $client = new Google_Client();
